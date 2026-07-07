cc3a3378b2f4dac92bcd73d2d71db90668453843
Sandeep <sandeepkumar20226ku@gmail.com>
2026-07-07 17:39:30 +0530

    axiosInstance update

--- client/src/components/utility/axiosInstance.js
+++ client/src/components/utility/axiosInstance.js
@@ -3,12 +3,9 @@ import axios from "axios";
 const DB_URL = import.meta.env.VITE_DB_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: [
-    DB_URL,
-    "https://chat-app-mern-full-stack-project.vercel.app/api/v1",
-  ],
+  baseURL: DB_URL,
   withCredentials: true,
   headers: {
-    ContentType: "application/json",
+    "Content-Type": "application/json",
   },
 });
